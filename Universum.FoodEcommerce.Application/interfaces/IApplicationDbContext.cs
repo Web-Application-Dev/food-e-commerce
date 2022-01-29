e4c389f91579b6d5287fc2aa04146e207500508d
Arlind Kasumi <akasumi61@gmail.com>
2022-01-29 18:14:25 +0100

    ndryshim ne application

---
+++ Universum.FoodEcommerce.Application/interfaces/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Universum.FoodEcommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universum.FoodEcommerce.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Products { get; set; }
+        Task<int> SaveChangesAsync();
+    }
+}
